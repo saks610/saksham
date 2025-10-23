1d81b8f99f2b499dae5d42fe5111037058ea3b74
saks610 <sakshamgaikwad1377@gmail.com>
2025-10-23 15:10:02 +0530

    Update sliding_window.cpp

--- sliding_window.cpp
+++ sliding_window.cpp
@@ -5,28 +5,27 @@ using namespace std;
 int maxSlidingWindowSum(vector<int>& arr, int k) {
     int n = arr.size();
 
-    // Step 1: Handle edge case if array size < k
+    
     if (n < k) {
         cout << "Window size is larger than the array!" << endl;
         return -1;
     }
 
-    // Step 2: Calculate the sum of the first window
+    
     int windowSum = 0;
     for (int i = 0; i < k; i++) {
         windowSum += arr[i];
     }
 
-    // Step 3: Initialize maxSum with first window sum
+    
     int maxSum = windowSum;
 
-    // Step 4: Slide the window one element at a time
+    
     for (int i = k; i < n; i++) {
-        // Add new element (arr[i])
-        // Remove the element that goes out of window (arr[i - k])
+        
+  window (arr[i - k])
         windowSum += arr[i] - arr[i - k];
-
-        // Update maxSum if current windowSum is bigger
+        
         maxSum = max(maxSum, windowSum);
     }
 
